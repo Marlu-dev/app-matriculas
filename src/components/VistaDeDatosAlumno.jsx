55cd6c3e8b77dfb44ab989eefea73098b1683e34
Marlu-dev <smarianolm@gmail.com>
2023-12-13 03:45:07 -0500

    agregado apoderado en vista

--- src/components/VistaDeDatosAlumno.jsx
+++ src/components/VistaDeDatosAlumno.jsx
@@ -28,6 +28,7 @@ const VistaDeDatosAlumno = ({ codigo }) => {
 
       if (alumnosData.length > 0) {
         setAlumno(alumnosData[0])
+        console.log(alumno)
       }
     })
 
@@ -39,37 +40,100 @@ const VistaDeDatosAlumno = ({ codigo }) => {
   return (
     <>
       <div>
-        <label>Nombre</label>
-        <label>{alumno.nombre}</label>
+        <h1>Alumno</h1>
+        <div>
+          <label>Nombre: </label>
+          <label>{alumno.nombre}</label>
+        </div>
+        <div>
+          <label>Apellido Paterno: </label>
+          <label>{alumno.apellidoPaterno}</label>
+        </div>
+        <div>
+          <label>Apellido Materno: </label>
+          <label>{alumno.apellidoMaterno}</label>
+        </div>
+        <div>
+          <label>DNI: </label>
+          <label>{alumno.dni}</label>
+        </div>
+        <div>
+          <label>Grupo: </label>
+          <label>{alumno.grupo}</label>
+        </div>
+        <div>
+          <label>Carrera: </label>
+          <label>{alumno.carrera}</label>
+        </div>
+        <div>
+          <label>Dirección: </label>
+          <label>{alumno.direccion}</label>
+        </div>
+        <div>
+          <label>Teléfono Celular: </label>
+          <label>{alumno.telefonoCelular}</label>
+        </div>
       </div>
+
       <div>
-        <label>Apellido Paterno</label>
-        <label>{alumno.apellidoPaterno}</label>
+        <h1>Apoderado</h1>
+        {alumno.apoderado
+          ? (
+            <div>
+              <label>Nombre: </label>
+              <label>{alumno.apoderado.nombreApoderado}</label>
+            </div>
+            )
+          : (
+            <label>No hay información de apoderado</label>
+            )}
+
+        {alumno.apoderado
+          ? (
+            <div>
+              <label>Apellido Paterno: </label>
+              <label>{alumno.apoderado.apellidoPaternoApoderado}</label>
+            </div>
+            )
+          : (
+            <label>No hay información de apoderado</label>
+            )}
+
+        {alumno.apoderado
+          ? (
+            <div>
+              <label>Apellido Materno: </label>
+              <label>{alumno.apoderado.apellidoMaternoApoderado}</label>
+            </div>
+            )
+          : (
+            <label>No hay información de apoderado</label>
+            )}
+
+        {alumno.apoderado
+          ? (
+            <div>
+              <label>DNI : </label>
+              <label>{alumno.apoderado.dniApoderado}</label>
+            </div>
+            )
+          : (
+            <label>No hay información de apoderado</label>
+            )}
+
+        {alumno.apoderado
+          ? (
+            <div>
+              <label>Teléfono Celular : </label>
+              <label>{alumno.apoderado.telefonoCelularApoderado}</label>
+            </div>
+            )
+          : (
+            <label>No hay información de apoderado</label>
+            )}
+
       </div>
-      <div>
-        <label>Apellido Materno</label>
-        <label>{alumno.apellidoMaterno}</label>
-      </div>
-      <div>
-        <label>DNI</label>
-        <label>{alumno.dni}</label>
-      </div>
-      <div>
-        <label>Grupo</label>
-        <label>{alumno.grupo}</label>
-      </div>
-      <div>
-        <label>Carrera</label>
-        <label>{alumno.carrera}</label>
-      </div>
-      <div>
-        <label>Dirección</label>
-        <label>{alumno.direccion}</label>
-      </div>
-      <div>
-        <label>Teléfono Celular</label>
-        <label>{alumno.telefonoCelular}</label>
-      </div>
+
       {/* <div>
         <label>Apoderado</label>
         <label>{alumno.apoderado}</label>
