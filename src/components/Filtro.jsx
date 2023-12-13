4635e3430f5ea5d1f6908a554878532711d9196b
Marlu-dev <smarianolm@gmail.com>
2023-12-13 03:16:23 -0500

    agregadas vista de alumno y test

--- src/components/Filtro.jsx
+++ src/components/Filtro.jsx
@@ -1,63 +1,63 @@
-import React, { useEffect, useState } from "react";
-import { collection, onSnapshot, query } from "firebase/firestore";
-import db from "../../public/services/firebase/firebase.js";
+import React, { useEffect, useState } from 'react'
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import db from '../../public/services/firebase/firebase.js'
 
 const Filtro = ({ nombre, coleccion, funcionObtenerFiltros }) => {
-  const [opciones, setOpciones] = useState([]);
-  const [busqueda, setBusqueda] = useState("");
-  const [seleccion, setSeleccion] = useState([]);
+  const [opciones, setOpciones] = useState([])
+  const [busqueda, setBusqueda] = useState('')
+  const [seleccion, setSeleccion] = useState([])
 
   useEffect(() => {
-    const q = query(collection(db, coleccion));
+    const q = query(collection(db, coleccion))
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const listaDeOpciones = querySnapshot.docs.map((doc) => ({
         id: doc.id,
-        nombre: doc.data().nombre,
-      }));
-      setOpciones(listaDeOpciones);
-      console.log(opciones);
-    });
+        nombre: doc.data().nombre
+      }))
+      setOpciones(listaDeOpciones)
+      console.log(opciones)
+    })
 
     // Desvincular el listener cuando el componente se desmonta
-    return () => unsubscribe();
-  }, []);
+    return () => unsubscribe()
+  }, [])
 
   const buscarOpcion = (e) => {
-    setBusqueda(e.target.value);
-  };
+    setBusqueda(e.target.value)
+  }
 
   useEffect(() => {
-    funcionObtenerFiltros(seleccion);
-  }, [seleccion]);
+    funcionObtenerFiltros(seleccion)
+  }, [seleccion])
 
   const filtrarBusqueda = (opciones, busqueda) => {
     return opciones.filter((opcion) => {
-      if (opcion.nombre && typeof opcion.nombre === "string") {
-        const busquedaLowerCase = busqueda.toLowerCase().trim();
-        const nombreOpcion = opcion.nombre.toLowerCase().trim();
+      if (opcion.nombre && typeof opcion.nombre === 'string') {
+        const busquedaLowerCase = busqueda.toLowerCase().trim()
+        const nombreOpcion = opcion.nombre.toLowerCase().trim()
         const regex = new RegExp(
           busquedaLowerCase
-            .split(" ")
+            .split(' ')
             .map((word) => `(?=.*\\b${word})`)
-            .join(""),
-          "i"
-        );
+            .join(''),
+          'i'
+        )
 
-        return regex.test(nombreOpcion);
+        return regex.test(nombreOpcion)
       } else {
-        return false; // O maneja esto de acuerdo a tus necesidades
+        return false // O maneja esto de acuerdo a tus necesidades
       }
-    });
-  };
+    })
+  }
 
   const checkboxSeleccionado = (e) => {
-    const { name, checked } = e.target;
+    const { name, checked } = e.target
     if (checked) {
-      setSeleccion([...seleccion, name]);
+      setSeleccion([...seleccion, name])
     } else {
-      setSeleccion(seleccion.filter((item) => item !== name));
+      setSeleccion(seleccion.filter((item) => item !== name))
     }
-  };
+  }
 
   //   console.log('seleccion', seleccion)
 
@@ -65,13 +65,13 @@ const Filtro = ({ nombre, coleccion, funcionObtenerFiltros }) => {
     <>
       <div>{nombre}</div>
       <div>
-        <input type="text" onChange={buscarOpcion} />
-        <div className="opciones" />
+        <input type='text' onChange={buscarOpcion} />
+        <div className='opciones' />
         {filtrarBusqueda(opciones, busqueda).map((opcion) => (
           <div key={opcion.id}>
             {opcion.nombre}
             <input
-              type="checkbox"
+              type='checkbox'
               name={opcion.nombre}
               id={opcion.id}
               onChange={checkboxSeleccionado}
@@ -80,7 +80,7 @@ const Filtro = ({ nombre, coleccion, funcionObtenerFiltros }) => {
         ))}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Filtro;
+export default Filtro
