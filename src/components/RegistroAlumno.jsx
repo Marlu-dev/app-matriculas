ad602c160ca04b0e11a90927a649a5df7c647638
JefreeMendoza <142171894+JefreeMendoza@users.noreply.github.com>
2023-12-14 14:00:18 -0500

    edicion final registrar alumno
    falta apoderado

--- src/components/RegistroAlumno.jsx
+++ src/components/RegistroAlumno.jsx
@@ -125,13 +125,15 @@ function RegistroAlumno () {
                 <div>
                   <label>Codigo</label>
                 </div>
-                <input
-                  name='codigo'
-                  id='codigo'
-                  value={codigoNuevo}
-                  readOnly
-                  disabled
-                />
+                <div className='codigo-sec'>
+                  <input
+                    name='codigo'
+                    id='codigo'
+                    value={codigoNuevo}
+                    readOnly
+                    disabled
+                  />
+                </div>
               </div>
 
               {/* SEGUNDO DATO: Grupo */}
@@ -139,34 +141,30 @@ function RegistroAlumno () {
                 <div>
                   <label>Grupo</label>
                 </div>
-                <div className='dropdwon'> 
-                  <div className='select-box'>
-                    <span className='label-select'>
-                      Opción:  
-                    </span>
+                <div className='main-dropdwon'> 
+                  <div className='select-container'>
                     <Select
                       coleccion='grupos'
                       nombre='grupo'
                       onSelectChange={handleChange}
                     />
+                    <div className='select-icon'>
+                    <svg 
+                      xmlns="http://www.w3.org/2000/svg" 
+                      class="icon icon-tabler icon-tabler-chevron-down" 
+                      width="25" 
+                      height="25" 
+                      viewBox="0 0 24 24" 
+                      stroke-width="3.5" 
+                      stroke="#2c3e50" 
+                      fill="none" 
+                      stroke-linecap="round" 
+                      stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M6 9l6 6l6 -6" />
+                    </svg>
+                    </div>
                   </div>
-                  <div className='icon-select'>
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        class="icon icon-tabler icon-tabler-square-rounded-chevron-down-filled" 
-                        width="30" 
-                        height="30" 
-                        viewBox="0 0 24 24" 
-                        stroke-width="1.5" 
-                        stroke="#2c3e50" 
-                        fill="none" 
-                        stroke-linecap="round" 
-                        stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M12 2c-.218 0 -.432 .002 -.642 .005l-.616 .017l-.299 .013l-.579 .034l-.553 .046c-4.785 .464 -6.732 2.411 -7.196 7.196l-.046 .553l-.034 .579c-.005 .098 -.01 .198 -.013 .299l-.017 .616l-.004 .318l-.001 .324c0 .218 .002 .432 .005 .642l.017 .616l.013 .299l.034 .579l.046 .553c.464 4.785 2.411 6.732 7.196 7.196l.553 .046l.579 .034c.098 .005 .198 .01 .299 .013l.616 .017l.642 .005l.642 -.005l.616 -.017l.299 -.013l.579 -.034l.553 -.046c4.785 -.464 6.732 -2.411 7.196 -7.196l.046 -.553l.034 -.579c.005 -.098 .01 -.198 .013 -.299l.017 -.616l.005 -.642l-.005 -.642l-.017 -.616l-.013 -.299l-.034 -.579l-.046 -.553c-.464 -4.785 -2.411 -6.732 -7.196 -7.196l-.553 -.046l-.579 -.034a28.058 28.058 0 0 0 -.299 -.013l-.616 -.017l-.318 -.004l-.324 -.001zm-3.707 8.293a1 1 0 0 1 1.32 -.083l.094 .083l2.293 2.292l2.293 -2.292a1 1 0 0 1 1.32 -.083l.094 .083a1 1 0 0 1 .083 1.32l-.083 .094l-3 3a1 1 0 0 1 -1.32 .083l-.094 -.083l-3 -3a1 1 0 0 1 0 -1.414z" fill="currentColor" stroke-width="0" />
-                      </svg>
-                  </div>
-
                 </div>
               </div>
 
@@ -175,33 +173,31 @@ function RegistroAlumno () {
                 <div>
                   <label>Carrera</label>
                 </div>
-                <div className='select-box'>
-                  <span className='label-select'>
-                    Opción:  
-                  </span>
-                  <Select
-                    coleccion={selectedGrupo}
-                    nombre='carrera'
-                    onSelectChange={handleChange}
-                  />
-                  <div className='icon-select'>
-                    <svg 
-                      xmlns="http://www.w3.org/2000/svg" 
-                      class="icon icon-tabler icon-tabler-square-rounded-chevron-down-filled" 
-                      width="30" 
-                      height="30" 
-                      viewBox="0 0 24 24" 
-                      stroke-width="1.5" 
-                      stroke="#2c3e50" 
-                      fill="none" 
-                      stroke-linecap="round" 
-                      stroke-linejoin="round">
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                      <path d="M12 2c-.218 0 -.432 .002 -.642 .005l-.616 .017l-.299 .013l-.579 .034l-.553 .046c-4.785 .464 -6.732 2.411 -7.196 7.196l-.046 .553l-.034 .579c-.005 .098 -.01 .198 -.013 .299l-.017 .616l-.004 .318l-.001 .324c0 .218 .002 .432 .005 .642l.017 .616l.013 .299l.034 .579l.046 .553c.464 4.785 2.411 6.732 7.196 7.196l.553 .046l.579 .034c.098 .005 .198 .01 .299 .013l.616 .017l.642 .005l.642 -.005l.616 -.017l.299 -.013l.579 -.034l.553 -.046c4.785 -.464 6.732 -2.411 7.196 -7.196l.046 -.553l.034 -.579c.005 -.098 .01 -.198 .013 -.299l.017 -.616l.005 -.642l-.005 -.642l-.017 -.616l-.013 -.299l-.034 -.579l-.046 -.553c-.464 -4.785 -2.411 -6.732 -7.196 -7.196l-.553 -.046l-.579 -.034a28.058 28.058 0 0 0 -.299 -.013l-.616 -.017l-.318 -.004l-.324 -.001zm-3.707 8.293a1 1 0 0 1 1.32 -.083l.094 .083l2.293 2.292l2.293 -2.292a1 1 0 0 1 1.32 -.083l.094 .083a1 1 0 0 1 .083 1.32l-.083 .094l-3 3a1 1 0 0 1 -1.32 .083l-.094 -.083l-3 -3a1 1 0 0 1 0 -1.414z" fill="currentColor" stroke-width="0" />
-                    </svg>
+                <div className="main-dropdwon">
+                  <div className='select-container'>
+                    <Select
+                      coleccion={selectedGrupo}
+                      nombre='carrera'
+                      onSelectChange={handleChange}
+                    />
+                    <div className='select-icon'>
+                      <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        class="icon icon-tabler icon-tabler-chevron-down" 
+                        width="25" 
+                        height="25" 
+                        viewBox="0 0 24 24" 
+                        stroke-width="3.5" 
+                        stroke="#2c3e50" 
+                        fill="none" 
+                        stroke-linecap="round" 
+                        stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M6 9l6 6l6 -6" />
+                      </svg>
+                    </div>
                   </div>
-                </div>
-                
+                </div>                
               </div>
 
               {/* CUARTO DATO: Nombre */}
